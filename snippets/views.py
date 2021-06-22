dec91fdb30772021bf7d7ca5548d680f59a2354c
Nevaland <deanh0716@gmail.com>
2021-06-22 14:52:25 +0900

    Refactoring

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,8 @@
-from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from django.contrib.auth.models import User
 
@@ -16,8 +14,7 @@ from snippets.permissions import IsOwnerOrReadOnly
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
